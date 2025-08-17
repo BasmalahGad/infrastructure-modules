350275af9d82a4b640e17dbe6ed4ef9ee52c44d8
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2025-08-17 19:56:34 +0300

    some changes

--- 03- variables.tf
+++ 03- variables.tf
@@ -28,6 +28,31 @@ variable "cluster_version" {
   type        = string
 }
 
+# variable "node_groups" {
+#   description = "EKS node groups configuration"
+#   type = map(object({
+#     instance_types = list(string)
+#     capacity_type  = string
+#     scaling_config = object({
+#       desired_size = number
+#       max_size     = number
+#       min_size     = number
+#     })
+#   }))
+
+#   default = {
+#     default = {
+#       instance_types = ["t3.medium"]
+#       capacity_type  = "ON_DEMAND"
+#       scaling_config = {
+#         desired_size = 2
+#         max_size     = 4
+#         min_size     = 1
+#       }
+#     }
+#   }
+# }
+
 variable "node_groups" {
   description = "EKS node groups configuration"
   type = map(object({
@@ -42,11 +67,11 @@ variable "node_groups" {
 
   default = {
     default = {
-      instance_types = ["t3.medium"]
-      capacity_type  = "ON_DEMAND"
+      instance_types = ["t3.micro"],
+      capacity_type  = "ON_DEMAND",
       scaling_config = {
-        desired_size = 2
-        max_size     = 4
+        desired_size = 2,
+        max_size     = 4,
         min_size     = 1
       }
     }
